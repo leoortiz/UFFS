d52a62daf8a5e35f5fc10e30a5da8ea569971994
Leonardo Ortiz <leoortiz@Unochapeco.edu.br>
2016-04-27 11:04:23 -0300

    Update pointintriangle.cpp
    sugar

--- 2016-1/Sistemas-Digitais/T3/pointintriangle.cpp
+++ 2016-1/Sistemas-Digitais/T3/pointintriangle.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <fstream>  // files
+#include <fstream>
 #include <cstdlib>  // rand
-#include <ctime>    // time
+#include <ctime>
 
-#define size 500
+#define sizeX 640
+#define sizeY 480
 
 using namespace std;
 
 typedef struct ponto{
-	int x, y;
+	int x;
+	int y;
 } ponto;
 
 typedef struct triangulo{
@@ -20,59 +22,78 @@ typedef struct triangulo{
 int sign(ponto a, ponto b, ponto c);
 int isInTriangle(ponto p, triangulo t);
 
+void writeInput (triangulo tri, ponto p);
 void writeOutput(triangulo tri, ponto p);
 
+fstream input;
 fstream output;
 
 int main() {
 	int i;
-	ponto p[size];
-	triangulo tri[size];
-	
+	triangulo tri[sizeX];
+	ponto p[sizeX];
+
 	srand( time(NULL) );
-	
+
+	input.open  ( "input.txt"  , ios::out );
 	output.open ( "output.txt" , ios::out );
-	
-	for(i = 0; i < size; i++) {
-		tri[i].a.x = rand() % size + 1;
-		tri[i].a.y = rand() % size + 1;
-		
-		tri[i].b.x = rand() % size + 1;
-		tri[i].b.y = rand() % size + 1;
-		
-		tri[i].c.x = rand() % size + 1;
-		tri[i].c.y = rand() % size + 1;
-		
-		p[i].x = rand() % size + 1;
-		p[i].y = rand() % size + 1;
-		
+
+	for( i = 0; i < sizeX; i++ ) {
+		tri[i].a.x = rand() % sizeX + 1;
+		tri[i].a.y = rand() % sizeY + 1;
+
+		tri[i].b.x = rand() % sizeX + 1;
+		tri[i].b.y = rand() % sizeY + 1;
+
+		tri[i].c.x = rand() % sizeX + 1;
+		tri[i].c.y = rand() % sizeY + 1;
+
+		p[i].x = rand() % sizeX + 1;
+		p[i].y = rand() % sizeY + 1;
+
+		writeInput (tri[i], p[i]);
 		writeOutput(tri[i], p[i]);
 	}
 
 	return 0;
 }
 
+void writeInput(triangulo tri, ponto p) {
+	input << "A(";
+	input << tri.a.x << ",";
+	input << tri.a.y << "), ";
+
+	input << "B(";
+	input << tri.b.x << ",";
+	input << tri.b.y << "), ";
+
+	input << "C(";
+	input << tri.c.x << ",";
+	input << tri.c.y << "), ";
+
+	input << "P(";
+	input << p.x << ",";
+	input << p.y << ")" << endl;
+}
+
 void writeOutput(triangulo tri, ponto p) {
 	output << "A(";
 	output << tri.a.x << ",";
 	output << tri.a.y << "), ";
-	
+
 	output << "B(";
 	output << tri.b.x << ",";
 	output << tri.b.y << "), ";
-	
+
 	output << "C(";
 	output << tri.c.x << ",";
 	output << tri.c.y << "), ";
-	
+
 	output << "P(";
 	output << p.x << ",";
 	output << p.y << ") \t= ";
-	
-	if( isInTriangle(p, tri) )
-		output << "Ponto dentro do triangulo" << endl;
-	else
-		output << "Ponto fora do triangulo" << endl;
+
+	output << isInTriangle(p, tri) << endl;
 }
 
 int sign(ponto a, ponto b, ponto c) {
@@ -83,6 +104,6 @@ int isInTriangle(ponto p, triangulo t) {
 	bool b1 = sign(p, t.a, t.b) < 0;
 	bool b2 = sign(p, t.b, t.c) < 0;
 	bool b3 = sign(p, t.c, t.a) < 0;
-	
+
 	return ( (b1 == b2) && (b2 == b3) );
 }
